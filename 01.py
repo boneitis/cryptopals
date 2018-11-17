699e0c318e4f368e4ec76b8f057ca9165c52428e
Jesse <half.lit.night@gmail.com>
2018-11-17 03:34:18 -0800

    02. huzzah!

--- 01.py
+++ 01.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 
+"""
+$ perl -e 'print "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";'|./01.py
+
+"""
+
+import sys
 import binascii
 import base64
 
-ct = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-pt = base64.b64encode(binascii.unhexlify(ct)).decode("utf-8")
+ct_nib = sys.stdin.read( 1024 )
+while ct_nib != "":
+	print( base64.b64encode(binascii.unhexlify(ct_nib)).decode("utf-8") )
+	ct_nib = sys.stdin.read( 1024 )
 
-print (pt)
