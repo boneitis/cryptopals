7ccc00889f49fdf1b5467cd9d54882161dfa197a
Jesse <half.lit.night@gmail.com>
2018-11-17 23:46:51 -0800

    folderize

---
+++ response/02.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+$ ./02.py <@param1> <@param2>
+
+Todo:
+  Break into blocks?
+
+"""
+
+import sys
+
+if len( sys.argv ) == 3:
+	if len( sys.argv[1] ) != len( sys.argv[2] ):
+		print( sys.argv[0] + ": Parameter length mismatch." )
+	else:
+		xorOut = hex( int(sys.argv[1], 16) ^ int(sys.argv[2], 16) )
+		print( xorOut.lstrip("0x").zfill(len(sys.argv[1])) )
+elif len( sys.argv ) == 1:
+	xorOut = hex( int("1c0111001f010100061a024b53535009181c", 16) ^ \
+		int("686974207468652062756c6c277320657965", 16) )
+	print( xorOut.lstrip("0x") )
+else:
+	print( "Usage: " + sys.argv[0] + " <@param1> <@param2>" )
+
