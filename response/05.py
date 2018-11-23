be9fc80f487ffb9bfad2f3ca4fd739c81fccffc7
js <some@handsome.guy>
2018-11-23 08:00:49 -0800

    05cleanup181123

--- response/05.py
+++ response/05.py
@@ -2,6 +2,7 @@
 
 """
 $ perl -e 'print "Burning \x27em, if you ain\x27t quick and nimble\nI go crazy when I hear a cymbal";' |./05.py
+
 """
 
 import sys
@@ -28,14 +29,10 @@ BUF_LIMIT_ALIGNED = BUF_LIMIT - (BUF_LIMIT % MOD_OPERAND)
 assert BUF_LIMIT_ALIGNED > 0
 assert MOD_OPERAND <= BUF_LIMIT_ALIGNED
 
-#print( 'key is: ' + KEY.decode('ISO-8859-1') )
-#print( 'key length is: ' + str( MOD_OPERAND ) )
-#print( 'bs is: ' + str(BUF_LIMIT) )
-
 pt = sys.stdin.buffer.read(BUF_LIMIT_ALIGNED)
 while pt != b'':
     mod_pad = len(pt) % MOD_OPERAND
     KEY_REPEATED = (KEY * int(len(pt)/MOD_OPERAND)) + KEY[:mod_pad]
-#    print(" key repeated: " + KEY_REPEATED.decode('ISO-8859-1') )
     print( ba.hexlify(strxor(pt, KEY_REPEATED)).decode('ISO-8859-1'), end='' )
     pt = sys.stdin.buffer.read(BUF_LIMIT_ALIGNED)
+
