dd5d0fafee46082eb0a9a33f58b7a90fe19354c3
rockstar <me@mydomain.com>
2018-11-25 12:29:40 -0800

    1811251230_05work

--- response/6test.py
+++ response/6test.py
@@ -10,13 +10,14 @@ for x in range(candidate_keylen):
 import sys
 import binascii as ba
 import base64 as b64
+#from s04aux import *
 from s06aux import *
 
-BUFFER_LEN = 3900
+BUFFER_LEN = 4096
 KEYSIZE_FLOOR = 2
 KEYSIZE_CEIL = 40
 HAMMING_PAIRS = 7
-
+#NO_PT_PER_KEYSIZE = 1
 #ct = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 ct = b64.b64decode( sys.stdin.buffer.read( BUFFER_LEN ) ).decode()
 
@@ -33,8 +34,10 @@ for keysize in range( KEYSIZE_FLOOR, KEYSIZE_CEIL+1 ):
         hamSumRaw += computeHamming( ct[ (x*pairOffset) : (x*pairOffset)+keysize ], ct[ (x*pairOffset)+keysize : (x*pairOffset)+(2*keysize) ] )
 #        print( "keysize " + str(keysize) + ", sum " + str(hamSumRaw) + ", on " + ct[ (x*pairOffset) : (x*pairOffset)+keysize ] + ", " + ct[ (x*pairOffset)+keysize : (x*pairOffset)+(2*keysize) ] )
     hamSumNorm = ( float(hamSumRaw) / keysize ) / HAMMING_PAIRS
-    print("keysize " + str(keysize) + ", normed sum " + str(round(hamSumNorm,2)))
+# succeeding for loop does same thing
+#    print("keysize " + str(keysize) + ", normed sum " + str(round(hamSumNorm,2)))
     ksns[keysize] = hamSumNorm
 for x in ksns:
     print(str(x) + ' : ' + str(round(ksns[x],2)))
 
+print( sorted(ksns, key=ksns.get)[0] )
