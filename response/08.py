80e049524a86721406e22ae0429a3e3462ca7c02
Jesse <boneitis@users.noreply.github.com>
2018-11-30 04:04:19 -0800

    Add files via upload

---
+++ response/08.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+cat challenge/8.txt |./08.py
+"""
+
+import fileinput as f
+from collections import defaultdict
+from pprint import pprint
+
+lineIndex = 0
+sumsDict = {}
+
+for line in f.input():
+  lineIndex += 1
+  inLineStringDict = defaultdict(int)
+
+  # add 1 to dict-value of current block as dict-key
+  x = 0
+  while x < len(line)-16:
+    inLineStringDict[line[x:x+16]] += 1
+    x += 16
+
+  # sum up all occurrences in the current line
+  lineSum = 0
+  for y in inLineStringDict:
+    if inLineStringDict[y] > 1:
+      lineSum += inLineStringDict[y]
+  sumsDict[lineIndex] = lineSum
+  if lineIndex == 133:
+    pprint( inLineStringDict )
+
+longestLineIndex = 0
+highSum = 0
+
+for high in sumsDict:
+  if sumsDict[high] > highSum:
+    highSum = sumsDict[high]
+    longestLineIndex = high
+
+print( 'longestLineIndex=' + str(longestLineIndex) )
+
