7a8783304679133ca92c73c163af5570f028262d
some guy <some@guy>
2018-12-03 07:34:20 -0800

    1812030734

---
+++ response/aux.py
@@ -0,0 +1,15 @@
+"""
+Export bytestring/array data in base-64.
+Do take care to pass in 3-byte-aligned input until EOF.
+"""
+
+import base64 as b64
+
+B64BLOCKSIZE = 1023
+
+assert(B64BLOCKSIZE%3 == 0)
+
+def b64out(bytesString):
+  for x in range( 0, len(bytesString), B64BLOCKSIZE ):
+    print( b64.b64encode( bytesString[x:x+B64BLOCKSIZE] ).decode(), end='' )
+
