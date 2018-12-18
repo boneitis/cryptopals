43705d581e803407f5adff32a34e07eb77508017
some guy <some@handsome.guy>
2018-12-18 02:52:18 -0800

    1812180250

--- response/s13.py
+++ response/s13.py
@@ -4,13 +4,6 @@ from Crypto.Cipher import AES
 import random as r
 r.seed()
 
-def kvparse(s):
-  s1 = s.split('&')
-  s2 = {}
-  for x in range( len(s1) ):
-    s2[s1[x].split('=')[0]] = s1[x].split('=')[1]
-  return s2
-
 class ECB_Oracle:
   def __init__( self ):
     self.blocksize = 16
@@ -20,19 +13,20 @@ class ECB_Oracle:
   def queryOracle( self, challenge ):                                                                                    
     return self.cipher.encrypt( pad(challenge, self.blocksize, style='pkcs7') )
 
+  # parse string into raw, 'processable' object
+  def kvparse(self, s):
+    s1 = s.split('&')
+    s2 = []
+    for x in s1:
+      s2.append( x.split('=') )
+    return s2
+
   def decrypt_and_parse( self, ct ):
     pt = unpad( self.cipher.decrypt(ct), self.blocksize, style='pkcs7' )
-    return kvparse( pt.decode() )
+    return self.kvparse( pt.decode() )
 
-def profile_for( email ):
-  # sanitize
-  email = email.replace('&', '').replace('=', '')
-
-  profile = []
-  profile.append( ['email', email] )
-  profile.append( ['uid', 10 ] )
-  profile.append( ['role', 'user' ] )
-
+# encode from raw model data
+def stitch( profile ):
   stitch = []
   for x in profile:
     stitch.append( x[0] + '=' + str(x[1]) )
@@ -40,15 +34,25 @@ def profile_for( email ):
 
   return profileOut
 
+def profile_for( email ):
+  # sanitize, then create default profile in raw, 'processable' form
+  email = email.replace('&', '').replace('=', '')
+  profile = []
+  profile.append( ['email', email] )
+  profile.append( ['uid', 10 ] )
+  profile.append( ['role', 'user' ] )
+
+  # send to stitch() for encoding, and return
+  return stitch(profile)
+
 
 def main():
   mary = ECB_Oracle()
   query1 = mary.queryOracle( profile_for('foo@bar.coadmin' + '\x0b'*11).encode() )
-#  print(query1)
   query2 = mary.queryOracle( profile_for('foo@bar.commm').encode() )
-#  print(query2)
+
   submit = query2[:32] + query1[16:32]
-  print(  mary.decrypt_and_parse(submit) )
+  print( stitch( mary.decrypt_and_parse(submit) ))
 
 if __name__ == '__main__':
   main()
