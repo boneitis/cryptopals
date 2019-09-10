cb30eef3113b1860f397ccc3c9ca09c28ef13240
some guy <half.lit.night@gmail.com>
2019-09-09 22:09:02 -0700

    solve29

--- response/s29.py
+++ response/s29.py
@@ -16,8 +16,8 @@ class SHA1_29:
     self.h2_0 = 0x98badcfe
     self.h3_0 = 0x10325476
     self.h4_0 = 0xc3d2e1f0
-###    self.key = choice(open('/usr/share/dict/words').read().splitlines()).encode()
-    self.key = b'AAAA'
+    self.key = choice(open('/usr/share/dict/words').read().splitlines()).encode()
+#    self.key = b'AAAA'
     self.known_pt = b'comment1=cooking%20MCs;userdata=foo;comment2=%20like%20a%20pound%20of%20bacon'
 
   def preprocess(self, m):
@@ -33,7 +33,7 @@ class SHA1_29:
     return m
 
   def digest(self, m, stage=None):
-    print('stage:', stage)
+#    print('stage:', stage)
     if stage is None:
       h0 = self.h0_0
       h1 = self.h1_0
@@ -59,9 +59,6 @@ class SHA1_29:
     chunks = []
     for i in range(0, len(m), 64):
       chunks.append(m[i:i+64])
-#    print('number of chunks:', len(chunks))
-
-#    print('len ', len(chunks), 'of chunks:', chunks, end='\n\n\n')
 
     for chunk in chunks:
       w = []
@@ -133,20 +130,20 @@ class SHA1_29:
     return self.digest(m) == t
 
   def authenticate_admin(self, m, t):
-    print('oracle auth: about to digest', self.key, 'and', m)
+#    print('oracle auth: about to digest', self.key, 'and', m)
     if self.digest(self.key + m) == t:
-      print('oracle: keylen ' + str(len(self.key)))
+#      print('oracle: keylen ' + str(len(self.key)))
       return b';admin=true' in m
     else:
-      print('oracle: auth failed, got:\n', self.digest(self.key + m), 'versus\n', t)
+#      print('oracle: auth failed, got:\n', self.digest(self.key + m), 'versus\n', t)
       return False
 
 def main():
-  x1 = b'Fox'                                          # dfcd3454bbea788a751a696c24d97009ca992d17
-  x2 = b'The red fox\njumps over\nthe blue dog'        # 008646bbfb7dcbe2823cacc76cd190b1ee6e3abc
-  x3 = b'The red fox\njumps ouer\nthe blue dog'        # 8fd8755878514f32d1c676b179a90da4aefe4819
-  x4 = b'The red fox\njumps oevr\nthe blue dog'        # fcd37fdb5af2c6ff915fd401c0a97d9a46affb45
-  x5 = b'The red fox\njumps oer\nthe blue dog'         # 8acad682d5884c754bf417997d88bcf892b96a6c
+#  x1 = b'Fox'                                          # dfcd3454bbea788a751a696c24d97009ca992d17
+#  x2 = b'The red fox\njumps over\nthe blue dog'        # 008646bbfb7dcbe2823cacc76cd190b1ee6e3abc
+#  x3 = b'The red fox\njumps ouer\nthe blue dog'        # 8fd8755878514f32d1c676b179a90da4aefe4819
+#  x4 = b'The red fox\njumps oevr\nthe blue dog'        # fcd37fdb5af2c6ff915fd401c0a97d9a46affb45
+#  x5 = b'The red fox\njumps oer\nthe blue dog'         # 8acad682d5884c754bf417997d88bcf892b96a6c
 
   mysha1 = SHA1_29()
 #  tag = mysha1.digest(x1)
@@ -169,7 +166,7 @@ def main():
   print('resume:\n', resume_tag)
 
 #  for guess_keylen in range(1, 24):
-  for guess_keylen in range(4,5):
+  for guess_keylen in range(1,24):
     glue_padding = b'\x80' + ((120 - (77 + guess_keylen + 1)) * b'\x00') + ((77 + guess_keylen)*8).to_bytes(8, 'big')
     query = original_message + glue_padding + new_message
     if mysha1.authenticate_admin(query, resume_tag):
@@ -178,18 +175,6 @@ def main():
       break
     else:
       print(str(guess_keylen) + '...kaboom')
-
-#  tag = mysha1.challenge(x2)
-#  print(tag.hex())
-
-#  tag = mysha1.challenge(x3)
-#  print(tag.hex())
-
-#  tag = mysha1.challenge(x4)
-#  print(tag.hex())
-
-#  tag = mysha1.challenge(x5)
-#  print(tag.hex())
 
 if __name__ == '__main__':
   main()
