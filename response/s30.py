5ab55848e6581d959164d1422a1d2b82a8f8f455
some guy <half.lit.night@gmail.com>
2019-09-13 02:48:26 -0700

    md4wtf

--- response/s30.py
+++ response/s30.py
@@ -33,7 +33,10 @@ class MD4_30:
     return m
 
   def f(self, X, Y, Z):
-    return (X & Y) | (((~X) & 0xffffffff) & Z)
+    print('f() received:', X, Y, Z)
+    retval = (X & Y) | (((~X) & 0xffffffff) & Z)
+    print('f() returning:', retval)
+    return retval
 
   def g(self, X, Y, Z):
     return (X & Y) | (X & Z) | (Y & Z)
@@ -44,120 +47,120 @@ class MD4_30:
   # A = (A + f(B,C,D) + X[i]) << s
   def round1(self, A, B, C, D, X):
     A = (A + self.f(B, C, D) + X[0]) % 0x100000000
-    A = (A << 3) & self.Mask
+    A = ((A << 3) & 0xfffffff8) | ((A >> 29) & 0x00000007)
     D = (D + self.f(A, B, C) + X[1]) % 0x100000000
-    D = (D << 7) & self.Mask
+    D = ((D << 7) & 0xffffff80) | ((D >> 25) & 0x0000007f)
     C = (C + self.f(D, A, B) + X[2]) % 0x100000000
-    C = (C << 11) & self.Mask
+    C = ((C << 11) & 0xfffff800) | ((C >> 21) & 0x000007ff)
     B = (B + self.f(C, D, A) + X[3]) % 0x100000000
-    B = (B << 19) & self.Mask
+    B = ((B << 19) & 0xfff80000) | ((B >> 13) & 0x0007ffff)
 
     A = (A + self.f(B, C, D) + X[4]) % 0x100000000
-    A = (A << 3) & self.Mask
+    A = ((A << 3) & 0xfffffff8) | ((A >> 29) & 0x00000007)
     D = (D + self.f(A, B, C) + X[5]) % 0x100000000
-    D = (D << 7) & self.Mask
+    D = ((D << 7) & 0xffffff80) | ((D >> 25) & 0x0000007f)
     C = (C + self.f(D, A, B) + X[6]) % 0x100000000
-    C = (C << 11) & self.Mask
+    C = ((C << 11) & 0xfffff800) | ((C >> 21) & 0x000007ff)
     B = (B + self.f(C, D, A) + X[7]) % 0x100000000
-    B = (B << 19) & self.Mask
+    B = ((B << 19) & 0xfff80000) | ((B >> 13) & 0x0007ffff)
 
     A = (A + self.f(B, C, D) + X[8]) % 0x100000000
-    A = (A << 3) & self.Mask
+    A = ((A << 3) & 0xfffffff8) | ((A >> 29) & 0x00000007)
     D = (D + self.f(A, B, C) + X[9]) % 0x100000000
-    D = (D << 7) & self.Mask
+    D = ((D << 7) & 0xffffff80) | ((D >> 25) & 0x0000007f)
     C = (C + self.f(D, A, B) + X[10]) % 0x100000000
-    C = (C << 11) & self.Mask
+    C = ((C << 11) & 0xfffff800) | ((C >> 21) & 0x000007ff)
     B = (B + self.f(C, D, A) + X[11]) % 0x100000000
-    B = (B << 19) & self.Mask
+    B = ((B << 19) & 0xfff80000) | ((B >> 13) & 0x0007ffff)
 
     A = (A + self.f(B, C, D) + X[12]) % 0x100000000
-    A = (A << 3) & self.Mask
+    A = ((A << 3) & 0xfffffff8) | ((A >> 29) & 0x00000007)
     D = (D + self.f(A, B, C) + X[13]) % 0x100000000
-    D = (D << 7) & self.Mask
+    D = ((D << 7) & 0xffffff80) | ((D >> 25) & 0x0000007f)
     C = (C + self.f(D, A, B) + X[14]) % 0x100000000
-    C = (C << 11) & self.Mask
+    C = ((C << 11) & 0xfffff800) | ((C >> 21) & 0x000007ff)
     B = (B + self.f(C, D, A) + X[15]) % 0x100000000
-    B = (B << 19) & self.Mask
+    B = ((B << 19) & 0xfff80000) | ((B >> 13) & 0x0007ffff)
 
     return [A, B, C, D]
 
   # A = (A + g(B,C,D) + X[i] + self.Magic2) << s
   def round2(self, A, B, C, D, X):
     A = (A + self.g(B, C, D) + X[0] + self.Magic2) % 0x100000000
-    A = (A << 3) & self.Mask
+    A = ((A << 3) & 0xfffffff8) | ((A >> 29) & 0x00000007)
     D = (D + self.g(A, B, C) + X[4] + self.Magic2) % 0x100000000
-    D = (D << 5) & self.Mask
+    D = ((D << 5) & 0xffffffe0) | ((D >> 27) & 0x0000001f)
     C = (C + self.g(D, A, B) + X[8] + self.Magic2) % 0x100000000
-    C = (C << 9) & self.Mask
+    C = ((C << 9) & 0xfffffe00) | ((C >> 23) & 0x000001ff)
     B = (B + self.g(C, D, A) + X[12] + self.Magic2) % 0x100000000
-    B = (B << 13) & self.Mask
+    B = ((B << 13) & 0xffffe000) | ((B >> 19) & 0x00001fff)
 
     A = (A + self.g(B, C, D) + X[1] + self.Magic2) % 0x100000000
-    A = (A << 3) & self.Mask
+    A = ((A << 3) & 0xfffffff8) | ((A >> 29) & 0x00000007)
     D = (D + self.g(A, B, C) + X[5] + self.Magic2) % 0x100000000
-    D = (D << 5) & self.Mask
+    D = ((D << 5) & 0xffffffe0) | ((D >> 27) & 0x0000001f)
     C = (C + self.g(D, A, B) + X[9] + self.Magic2) % 0x100000000
-    C = (C << 9) & self.Mask
+    C = ((C << 9) & 0xfffffe00) | ((C >> 23) & 0x000001ff)
     B = (B + self.g(C, D, A) + X[13] + self.Magic2) % 0x100000000
-    B = (B << 13) & self.Mask
+    B = ((B << 13) & 0xffffe000) | ((B >> 19) & 0x00001fff)
 
     A = (A + self.g(B, C, D) + X[2] + self.Magic2) % 0x100000000
-    A = (A << 3) & self.Mask
+    A = ((A << 3) & 0xfffffff8) | ((A >> 29) & 0x00000007)
     D = (D + self.g(A, B, C) + X[6] + self.Magic2) % 0x100000000
-    D = (D << 5) & self.Mask
+    D = ((D << 5) & 0xffffffe0) | ((D >> 27) & 0x0000001f)
     C = (C + self.g(D, A, B) + X[10] + self.Magic2) % 0x100000000
-    C = (C << 9) & self.Mask
+    C = ((C << 9) & 0xfffffe00) | ((C >> 23) & 0x000001ff)
     B = (B + self.g(C, D, A) + X[14] + self.Magic2) % 0x100000000
-    B = (B << 13) & self.Mask
+    B = ((B << 13) & 0xffffe000) | ((B >> 19) & 0x00001fff)
 
     A = (A + self.g(B, C, D) + X[3] + self.Magic2) % 0x100000000
-    A = (A << 3) & self.Mask
+    A = ((A << 3) & 0xfffffff8) | ((A >> 29) & 0x00000007)
     D = (D + self.g(A, B, C) + X[7] + self.Magic2) % 0x100000000
-    D = (D << 5) & self.Mask
+    D = ((D << 5) & 0xffffffe0) | ((D >> 27) & 0x0000001f)
     C = (C + self.g(D, A, B) + X[11] + self.Magic2) % 0x100000000
-    C = (C << 9) & self.Mask
+    C = ((C << 9) & 0xfffffe00) | ((C >> 23) & 0x000001ff)
     B = (B + self.g(C, D, A) + X[15] + self.Magic2) % 0x100000000
-    B = (B << 13) & self.Mask
+    B = ((B << 13) & 0xffffe000) | ((B >> 19) & 0x00001fff)
 
     return [A, B, C, D]
   
   # A = (A + h(B,C,D) + X[i] + self.Magic3) << s
   def round3(self, A, B, C, D, X):
     A = (A + self.h(B, C, D) + X[0] + self.Magic3) % 0x100000000
-    A = (A << 3) & self.Mask
+    A = ((A << 3) & 0xfffffff8) | ((A >> 29) & 0x00000007)
     D = (D + self.h(A, B, C) + X[8] + self.Magic3) % 0x100000000
-    D = (D << 9) & self.Mask
+    D = ((D << 9) & 0xfffffe00) | ((D >> 23) & 0x000001ff)
     C = (C + self.h(D, A, B) + X[4] + self.Magic3) % 0x100000000
-    C = (C << 11) & self.Mask
+    C = ((C << 11) & 0xfffff800) | ((C >> 21) & 0x000007ff)
     B = (B + self.h(C, D, A) + X[12] + self.Magic3) % 0x100000000
-    B = (B << 15) & self.Mask
+    B = ((B << 15) & 0xffffe000) | ((B >> 17) & 0x0001ffff)
 
     A = (A + self.h(B, C, D) + X[2] + self.Magic3) % 0x100000000
-    A = (A << 3) & self.Mask
+    A = ((A << 3) & 0xfffffff8) | ((A >> 29) & 0x00000007)
     D = (D + self.h(A, B, C) + X[10] + self.Magic3) % 0x100000000
-    D = (D << 9) & self.Mask
+    D = ((D << 9) & 0xfffffe00) | ((D >> 23) & 0x000001ff)
     C = (C + self.h(D, A, B) + X[6] + self.Magic3) % 0x100000000
-    C = (C << 11) & self.Mask
+    C = ((C << 11) & 0xfffff800) | ((C >> 21) & 0x000007ff)
     B = (B + self.h(C, D, A) + X[14] + self.Magic3) % 0x100000000
-    B = (B << 15) & self.Mask
+    B = ((B << 15) & 0xffffe000) | ((B >> 17) & 0x0001ffff)
 
     A = (A + self.h(B, C, D) + X[1] + self.Magic3) % 0x100000000
-    A = (A << 3) & self.Mask
+    A = ((A << 3) & 0xfffffff8) | ((A >> 29) & 0x00000007)
     D = (D + self.h(A, B, C) + X[9] + self.Magic3) % 0x100000000
-    D = (D << 9) & self.Mask
+    D = ((D << 9) & 0xfffffe00) | ((D >> 23) & 0x000001ff)
     C = (C + self.h(D, A, B) + X[5] + self.Magic3) % 0x100000000
-    C = (C << 11) & self.Mask
+    C = ((C << 11) & 0xfffff800) | ((C >> 21) & 0x000007ff)
     B = (B + self.h(C, D, A) + X[13] + self.Magic3) % 0x100000000
-    B = (B << 15) & self.Mask
+    B = ((B << 15) & 0xffffe000) | ((B >> 17) & 0x0001ffff)
 
     A = (A + self.h(B, C, D) + X[3] + self.Magic3) % 0x100000000
-    A = (A << 3) & self.Mask
+    A = ((A << 3) & 0xfffffff8) | ((A >> 29) & 0x00000007)
     D = (D + self.h(A, B, C) + X[11] + self.Magic3) % 0x100000000
-    D = (D << 9) & self.Mask
+    D = ((D << 9) & 0xfffffe00) | ((D >> 23) & 0x000001ff)
     C = (C + self.h(D, A, B) + X[7] + self.Magic3) % 0x100000000
-    C = (C << 11) & self.Mask
+    C = ((C << 11) & 0xfffff800) | ((C >> 21) & 0x000007ff)
     B = (B + self.h(C, D, A) + X[15] + self.Magic3) % 0x100000000
-    B = (B << 15) & self.Mask
+    B = ((B << 15) & 0xffffe000) | ((B >> 17) & 0x0001ffff)
 
     return [A, B, C, D]
 
@@ -205,12 +208,17 @@ class MD4_30:
         X.append(int.from_bytes(chunk[(j * 4) : (j*4)+4 ], 'little'))
       print('chunk:', chunk)
       print('X:', X, end='\n\n')
+      print('Before Anything:\nA:', A, 'B:', B, 'C:', C, 'D:', D)
 
       ABCD = self.round1(A, B, C, D, X)
+      print('Round 1 results:\nA:', ABCD[0], 'B:', ABCD[1], 'C:', ABCD[2], 'D:', ABCD[3])
       ABCD = self.round2(ABCD[0], ABCD[1], ABCD[2], ABCD[3], X)
+      print('Round 2 results:\nA:', ABCD[0], 'B:', ABCD[1], 'C:', ABCD[2], 'D:', ABCD[3])
       ABCD = self.round3(ABCD[0], ABCD[1], ABCD[2], ABCD[3], X)
+      print('Round 3 results:\nA:', ABCD[0], 'B:', ABCD[1], 'C:', ABCD[2], 'D:', ABCD[3])
 
       A = (ABCD[0] + AA) % 0x100000000
+      print('Round 3 results:\nA:', A, 'B:', B, 'C:', C, 'D:', D)
       B = (ABCD[1] + BB) % 0x100000000
       C = (ABCD[2] + CC) % 0x100000000
       D = (ABCD[3] + DD) % 0x100000000
@@ -234,7 +242,7 @@ def main():
 
   md4 = MD4_30()
 #  ret = md4.digest(b'A' * 32 + b'B' * 64 + b'C' * 64 + b'D')
-  ret = md4.digest(x3)
+  ret = md4.digest(x7)
   print(ret.hex())
 
 if __name__ == '__main__':
