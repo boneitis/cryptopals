7cf24bd3b808bc2d371699589ab6e662d8b23bd9
some guy <half.lit.night@gmail.com>
2019-09-16 13:39:32 -0700

    solve33

---
+++ response/s33.py
@@ -0,0 +1,22 @@
+'''
+$ python3 s33.py
+
+'''
+from Crypto.Random.random import randint
+
+
+#p = 37
+#g = 5
+p = 0xffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552bb9ed529077096966d670c354e4abc9804f1746c08ca237327ffffffffffffffff
+g = 2
+
+a = randint(2, p-2) % p
+A = pow(g, a, p)
+b = randint(2, p-2) % p
+B = pow(g, b, p)
+
+s1 = pow(B, a, p)
+s2 = pow(A, b, p)
+
+print(s1 == s2)
+
