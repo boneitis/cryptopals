a7c1dad62bb89f3f9c5712fac49c9a3dfb7deac7
some guy <half.lit.night@gmail.com>
2019-09-25 04:26:25 -0700

    why is this working?

--- response/s31.py
+++ response/s31.py
@@ -3,54 +3,31 @@ $ python3 s31.py
 
 '''
 
-from binascii import unhexlify
-from Crypto.Random import get_random_bytes
-from Crypto.Random.random import randint
-from Crypto.Util.strxor import strxor_c
-from s29 import SHA1_29
+import time
+import urllib.request
 
-class HMAC_31:
-  def __init__(self, key = get_random_bytes(20), m = b'hello world'):
-    self.key = key
-#   print('key:', self.key)
-    self.m = m
-#   print('m:', self.m)
-    self.hash = SHA1_29()
-    self.bs = 64
-    self.output_size = 20
-    if len(self.key) > self.bs:
-      self.key = self.hash.digest(self.key)
-    if len(self.key) < self.bs:
-#      print('class31: appending', (0).to_bytes((self.bs - len(self.key)), 'little'), 'to key')
-      self.key = self.key + (0).to_bytes((self.bs - len(self.key)), 'little')
-    self.o_key_pad = strxor_c(self.key, 0x5c)
-    self.i_key_pad = strxor_c(self.key, 0x36)
-#    print('class31: ipad', self.i_key_pad.hex())
-#    print('class31: opad', self.o_key_pad.hex())
-#   print(self.hash.digest(self.m).hex())
+def recover_next_byte(recovered_bytes):
+  hexa = '0123456789abcdef'
+  best_time = 100 # sentinel
+  for letter in hexa:
+    print('polling')
+    start = time.perf_counter()
+    try:
+      r = urllib.request.urlopen('http://localhost:9000/test?file=foo&signature=' + recovered_bytes + letter)
+      return letter
+    except urllib.error.HTTPError as e:
+      end = time.perf_counter()
+      if end - start < best_time:
+        best_time = end - start
+        candidate = letter
+  return letter
 
-  def hmac_digest(self):
-#    print('class31: inner hash digesting:', self.i_key_pad.hex(), 'cat', self.m)
-#    print('class31: outer hash digesting:', self.o_key_pad.hex(), 'cat', self.hash.digest(self.i_key_pad + self.m).hex())
-#   print('class31: passing to inner:', (self.i_key_pad + self.m))
-    return self.hash.digest(    self.o_key_pad + self.hash.digest(self.i_key_pad + self.m)    )
 
-testvectors = \
-[
-  [ (0x0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b).to_bytes(20, 'big'),          b'Hi There',                              b'b617318655057264e28bc0b6fb378c8ef146be00' ], \
-  [ b'Jefe',                                                                   b'what do ya want for nothing?',          b'effcdf6ae5eb2fa2d27416d5f184df9c259a7c79' ], \
-  [ (0xaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa).to_bytes(20, 'big'),  unhexlify(b'dd' * 50),                            b'125d7342b9ac11cd91a39af48aa17b4f63f175d3' ], \
-  [ (0x0102030405060708090a0b0c0d0e0f10111213141516171819).to_bytes(25, 'big'), (b'\xcd' * 50),                          b'4c9007f4026250c6bc8414f9bf50c86c2d7235da' ], \
-    # Succeeding line: Digest truncation not implemented
-  [ (0x0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c).to_bytes(20, 'big'),  b'Test With Truncation',                          b'4c1a03424b55e07fe7f27be1d58bb9324a9a5a04' ], \
-  [ unhexlify(b'aa' * 80),                                   b'Test Using Larger Than Block-Size Key - Hash Key First',  b'aa4ae5e15272d00e95705637ce8a3b55ed402112' ], \
-  [ unhexlify(b'aa' * 80),                 b'Test Using Larger Than Block-Size Key and Larger Than One Block-Size Data', b'e8e99d0f45237d786d6bbaa7965c7808bbff1a91' ] \
-]
 def main():
-  for i in testvectors:
-    myhmac = HMAC_31(i[0], i[1])
-    o = myhmac.hmac_digest()
-    print('\nfinalmac:', o.hex())
+  recovered_bytes = ''
+  for i in range(40):
+    recovered_bytes += recover_next_byte(recovered_bytes)
+    print('recovered: ' + recovered_bytes)
 
 if __name__ == '__main__':
   main()
---
+++ response/s31_server.py
@@ -0,0 +1,121 @@
+'''
+$ python3 s31.py
+
+Much credit to GH user `akalin` and their solutions as supplemental material for web/sockets refresher.
+
+'''
+
+from binascii import hexlify, unhexlify
+from Crypto.Random import get_random_bytes
+from Crypto.Random.random import randint
+from Crypto.Util.strxor import strxor_c
+import time
+import socketserver
+import http.server
+from urllib import parse
+from s29 import SHA1_29
+
+def generate_foo():
+  rand_len = randint(1, 100)
+  s = b''
+
+  for i in range(rand_len):
+    s += randint(32, 126).to_bytes(1, 'little')
+
+  f = open('foo', 'w')
+  f.write(s.decode('utf-8'))
+  f.close()
+
+class Handler(http.server.BaseHTTPRequestHandler):
+  def do_GET(self):
+    validated = False
+    print('raw path:', self.path)
+    u = parse.urlparse(self.path)
+    if u.path == '/test':
+      q = parse.parse_qs(u.query)
+      if 'file' in q and 'signature' in q and q['file'][0] == 'foo':
+        f = open('foo', 'rb')
+        m = f.read(256)
+        hmac = HMAC_31()
+        checksum = hexlify(hmac.hmac_digest(hmac.key + m)).decode('utf-8')
+        validated = self.insecure_compare(checksum, q['signature'][0])
+    if validated == True:
+      self.send_error(200)
+    else:
+      self.send_error(500)
+
+  def insecure_compare(self, a, b):
+    for i, j in zip(a.lower(), b.lower()):
+      if i == j:
+        pass
+      else:
+        return False
+      time.sleep(0.05)
+    return True
+
+class HMAC_31:
+  def __init__(self, key = (0x0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b).to_bytes(20, 'big')):
+    self.key = key
+    self.m = b'Hi There'
+    self.hash = SHA1_29()
+    self.bs = 64
+    self.output_size = 20
+    if len(self.key) > self.bs:
+      self.key = self.hash.digest(self.key)
+    if len(self.key) < self.bs:
+      self.key = self.key + (0).to_bytes((self.bs - len(self.key)), 'little')
+    self.o_key_pad = strxor_c(self.key, 0x5c)
+    self.i_key_pad = strxor_c(self.key, 0x36)
+#    self.foo_digest = self.hash.digest(self.key + readFileFoo)
+
+  def hmac_digest(self, m = b'Hi There'):
+#    print('class31: inner hash digesting:', self.i_key_pad.hex(), 'cat', self.m)
+#    print('class31: outer hash digesting:', self.o_key_pad.hex(), 'cat', self.hash.digest(self.i_key_pad + self.m).hex())
+#   print('class31: passing to inner:', (self.i_key_pad + self.m))
+    return self.hash.digest(    self.o_key_pad + self.hash.digest(self.i_key_pad + self.m)    )
+
+#  def authenticate_file_request(self, fname, signature):
+#    f = open('foo', 'rb')
+#    m = f.read(256)
+#    validator = HMAC_31()
+#    sig_calculated = validator.digest(m)
+
+
+testvectors = \
+[
+  [ 1, (0x0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b).to_bytes(20, 'big'),          b'Hi There',                              b'b617318655057264e28bc0b6fb378c8ef146be00' ], \
+  [ 2, b'Jefe',                                                                   b'what do ya want for nothing?',          b'effcdf6ae5eb2fa2d27416d5f184df9c259a7c79' ], \
+  [ 3, (0xaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa).to_bytes(20, 'big'),  unhexlify(b'dd' * 50),                            b'125d7342b9ac11cd91a39af48aa17b4f63f175d3' ], \
+  [ 4, (0x0102030405060708090a0b0c0d0e0f10111213141516171819).to_bytes(25, 'big'), (b'\xcd' * 50),                          b'4c9007f4026250c6bc8414f9bf50c86c2d7235da' ], \
+    # Succeeding line: Digest truncation not implemented
+  [ 5, (0x0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c).to_bytes(20, 'big'),  b'Test With Truncation',                          b'4c1a03424b55e07fe7f27be1d58bb9324a9a5a04' ], \
+  [ 6, unhexlify(b'aa' * 80),                                   b'Test Using Larger Than Block-Size Key - Hash Key First',  b'aa4ae5e15272d00e95705637ce8a3b55ed402112' ], \
+  [ 7, unhexlify(b'aa' * 80),                 b'Test Using Larger Than Block-Size Key and Larger Than One Block-Size Data', b'e8e99d0f45237d786d6bbaa7965c7808bbff1a91' ] \
+]
+def main():
+#  for a, b, c, d in testvectors:
+#    print('Test case #' + str(a) + ': ', end='')
+#    myhmac = HMAC_31(b, c)
+#    o = myhmac.hmac_digest()
+#    if o == unhexlify(d):
+#      print('huzzah')
+#    else:
+#      print('kaboom')
+
+#  generate_foo()
+
+  HOST, PORT = 'localhost', 9000
+
+  socketserver.TCPServer.allow_reuse_address = True
+  ss = socketserver.TCPServer((HOST, PORT), Handler)
+  try:
+    ss.serve_forever()
+  except KeyboardInterrupt:
+    print('\nCaught KeyboardInterrupt. Closing server.')
+    ss.server_close()
+  except:
+    print('Eek!')
+
+if __name__ == '__main__':
+  main()
+
