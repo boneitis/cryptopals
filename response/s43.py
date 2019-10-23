c70c5b4d4552a3bda1762fcc42fd846498244613
some guy <half.lit.night@gmail.com>
2019-10-22 19:16:10 -0700

    solve43

--- response/s43.py
+++ response/s43.py
@@ -1,7 +1,6 @@
 '''
 $ python3 s43.py
 
-ToDo: Grok the math.
 
 '''
 
@@ -62,15 +61,26 @@ if __name__ == '__main__':
   else:
     print('False')
 
-  print('\nproblem description params')
+  print('Problem solving...\n\n')
+
+  p = int.from_bytes(unhexlify(b'800000000000000089e1855218a0e7dac38136ffafa72eda7859f2171e25e65eac698c1702578b07dc2a1076da241c76c62d374d8389ea5aeffd3226a0530cc565f3bf6b50929139ebeac04f48c3c84afb796d61e5a4f9a8fda812ab59494232c7d2b4deb50aa18ee9e132bfa85ac4374d7f9091abc3d015efc871a584471bb1'), 'big')
+  q = int.from_bytes(unhexlify(b'f4f47f05794b256174bba6e9b396a7707e563c5b'), 'big')
+  g = int.from_bytes(unhexlify(b'5958c9d3898b224b12672c0b98e06c60df923cb8bc999d119458fef538b8fa4046c8db53039db620c094c9fa077ef389b5322a559946a71903f990f1f7e0e025e2d7f7cf494aff1a0470f5b64c36b625a097f1651fe775323556fe00b3608c887892878480e99041be601a62166ca6894bdd41a7054ec89f756ba9fc95302291'), 'big')
+
   r = 548099063082341131477253921760299949438196259240
   s = 857042759984254168557880549501802188789837994940
-  s_target = '0954edd5e0afe5542a4adf012611a91912a3ec16'
-  for i in range(2**16 - 1):
-    print(i)
-    gal = Entity_43(i)
-    r, s = gal.sign(gal.x)
-    if s == 857042759984254168557880549501802188789837994940:
-      print('match at i = ' + str(i))
-      break
+  try:
+    for k_ephemeral in range(2**16 - 1):
+      r_bruteforce = pow(g, k_ephemeral, p) % q
+      if r_bruteforce == r:
+        k_e = k_ephemeral
+        break
+    print('Ephemeral key: ' + str(k_e))
+  except:
+    print('kaboom')
+
+  x = b'For those that envy a MC it can be hazardous to your health\nSo be friendly, a matter of life and death, just like a etch-a-sketch\n'
+
+  d = ((s * k_e) - int.from_bytes(SHA1.new(x).digest(), 'big')) * invmod(r, q)
+  print('Private key: ' + str(d%q))
 
