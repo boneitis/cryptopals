6e9580568807bfd6375fb7d9bffb930c84428239
someguy <half.lit.night@gmail.com>
2020-04-14 22:48:43 -0700

    solve52

---
+++ response/s52.py
@@ -0,0 +1,110 @@
+'''
+$ python3 s52.py
+
+
+'''
+
+from s52_aux import *
+from Crypto.Cipher import AES
+from Crypto.Hash import MD5
+from Crypto.Util.Padding import pad
+from Crypto.Random import get_random_bytes as rand
+#from binascii import hexlify
+
+def f(M, H = AES.new(b'YELLOW SUBMARINE', AES.MODE_ECB).encrypt(b'\x34\x12\x80' + (b'\x00' * 13))[:2] + (b'\x80' + b'\x00' * 13)):
+  if len(H) < 16:
+    H = pad(H, 16, 'iso7816')
+  if len(M) % 16 != 0:
+    M = pad(M, 16, 'iso7816')
+  blocks = []
+  for i in range(len(M) // 16):
+    blocks.append(M[i*16:(i*16)+16])
+  for block in blocks:
+    aes = AES.new(H, AES.MODE_ECB)
+    H = aes.encrypt(block)[:2] + b'\x80' + (b'\x00' * 13)
+  return H[:2]
+
+def g(M, H = AES.new(b'YELLOW SUBMARINE', AES.MODE_ECB).encrypt(b'\x21\x43\x54\x80' + (b'\x00' * 12))[:3] + (b'\x80' + b'\x00' * 12)):
+  if len(H) < 16:
+    H = pad(H, 16, 'iso7816')
+  if len(M) % 16 != 0:
+    M = pad(M, 16, 'iso7816')
+  blocks = []
+  for i in range(len(M) // 16):
+    blocks.append(M[i*16:(i*16)+16])
+  for block in blocks:
+    aes = AES.new(H, AES.MODE_ECB)
+    H = aes.encrypt(block)[:3] + b'\x80' + (b'\x00' * 12)
+  return H[:3]
+
+def collide(M_chain = None):
+  while True:
+    sample0 = rand(16)
+    sample1 = rand(16)
+    if sample1 == sample0:
+      continue
+    if M_chain is not None:
+      H_0 = f(sample0, M_chain)
+      H_1 = f(sample1, M_chain)
+      if H_0 == H_1:
+        return [sample0, sample1, f(sample0, M_chain)]
+    else:
+      H_0 = f(sample0)
+      H_1 = f(sample1)
+      if H_0 == H_1:
+        return [sample0, sample1, f(sample0)]
+
+def main():
+  collisions = []
+  collisions.append(collide())
+
+  print('pre')
+  for b in range(12):
+    print('b', b + 1)
+    collisions.append(collide(collisions[-1][2]))
+
+  g_calls = 0
+  rev_lookup = {}
+  for collision in coll4096(collisions):
+    g_calls += 1
+    lookup = g(collision)
+    if lookup not in rev_lookup:
+      rev_lookup[lookup] = collision
+    else:
+      print('found\n', collision, '\n', rev_lookup.get(lookup))
+      print('4096-collision: g calls', g_calls, end='\n\n')
+      exit()
+
+  print('\n\n4096-collision: g calls', g_calls, end='\n\n')
+  print('length', len(rev_lookup))
+
+
+  g_calls = 0
+  rev_lookup = {}
+  for collision in coll8192(collisions):
+    g_calls += 1
+    lookup = g(collision)
+    if lookup not in rev_lookup:
+      rev_lookup[lookup] = collision
+    else:
+      print('found\n', collision, '\n', rev_lookup.get(lookup))
+      print('8102-collision: g calls', g_calls)
+      exit()
+                                                                                                                                   
+  print('\n\n8192-collision: g calls', g_calls, end='\n\n')
+  print('length', len(rev_lookup))
+
+
+  for collision in collisions:
+    print(collision)
+  print('\n')
+
+  print('failed to find g-collision.')
+
+#  print('count', trampoline(collisions))
+
+
+
+if __name__ == '__main__':
+  main()
+
---
+++ response/s52_aux.py
@@ -0,0 +1,361 @@
+import s52
+
+'''
+def coll2(vector):
+  print('  2col')
+  for i in range(2):
+    yield vector[0][i]
+
+def coll4(vector):
+  print('  4col')
+  for i in range(2):
+    b0 = vector[0]
+    for j in range(2):
+      b1 = vector[1]
+      yield b0[i] + b1[j]
+
+def coll8(vector):
+  print('  8col')
+  for i in range(2):
+    b0 = vector[0]
+    for j in range(2):
+      b1 = vector[1]
+      for k in range(2):
+        b2 = vector[2]
+        yield b0[i] + b1[j] + b2[k]
+
+def coll16(vector):
+  print('  16col')
+  for i in range(2):
+    b0 = vector[0]
+    for j in range(2):
+      b1 = vector[1]
+      for k in range(2):
+        b2 = vector[2]
+        for l in range(2):
+          b3 = vector[3]
+          yield b0[i] + b1[j] + b2[k] + b3[l]
+
+def coll32(vector):
+  print('  32col')
+  for i in range(2):
+    b0 = vector[0]
+    for j in range(2):
+      b1 = vector[1]
+      for k in range(2):
+        b2 = vector[2]
+        for l in range(2):
+          b3 = vector[3]
+          for m in range(2):
+            b4 = vector[4]
+            yield b0[i] + b1[j] + b2[k] + b3[l] + b4[m]
+
+def coll64(vector):
+  print('  64col')
+  for i in range(2):
+    b0 = vector[0]
+    for j in range(2):
+      b1 = vector[1]
+      for k in range(2):
+        b2 = vector[2]
+        for l in range(2):
+          b3 = vector[3]
+          for m in range(2):
+            b4 = vector[4]
+            for n in range(2):
+              b5 = vector[5]
+              yield b0[i] + b1[j] + b2[k] + b3[l] + b4[m] + b5[n]
+
+def coll128(vector):
+  print('  128col')
+  for i in range(2):
+    b0 = vector[0]
+    for j in range(2):
+      b1 = vector[1]
+      for k in range(2):
+        b2 = vector[2]
+        for l in range(2):
+          b3 = vector[3]
+          for m in range(2):
+            b4 = vector[4]
+            for n in range(2):
+              b5 = vector[5]
+              for o in range(2):
+                b6 = vector[6]
+                yield b0[i] + b1[j] + b2[k] + b3[l] + b4[m] + b5[n] + b6[o]
+
+def coll256(vector):
+  print('  256col')
+  for i in range(2):
+    b0 = vector[0]
+    for j in range(2):
+      b1 = vector[1]
+      for k in range(2):
+        b2 = vector[2]
+        for l in range(2):
+          b3 = vector[3]
+          for m in range(2):
+            b4 = vector[4]
+            for n in range(2):
+              b5 = vector[5]
+              for o in range(2):
+                b6 = vector[6]
+                for p in range(2):
+                  b7 = vector[7]
+                  yield b0[i] + b1[j] + b2[k] + b3[l] + b4[m] + b5[n] + b6[o] + b7[p]
+
+def coll512(vector):
+  print('  512col')
+  for i in range(2):
+    b0 = vector[0]
+    for j in range(2):
+      b1 = vector[1]
+      for k in range(2):
+        b2 = vector[2]
+        for l in range(2):
+          b3 = vector[3]
+          for m in range(2):
+            b4 = vector[4]
+            for n in range(2):
+              b5 = vector[5]
+              for o in range(2):
+                b6 = vector[6]
+                for p in range(2):
+                  b7 = vector[7]
+                  for q in range(2):
+                    b8 = vector[8]
+                    yield b0[i] + b1[j] + b2[k] + b3[l] + b4[m] + b5[n] + b6[o] + b7[p] + b8[q]
+
+def coll1024(vector):
+  print('  1024col')
+  for i in range(2):
+    b0 = vector[0]
+    for j in range(2):
+      b1 = vector[1]
+      for k in range(2):
+        b2 = vector[2]
+        for l in range(2):
+          b3 = vector[3]
+          for m in range(2):
+            b4 = vector[4]
+            for n in range(2):
+              b5 = vector[5]
+              for o in range(2):
+                b6 = vector[6]
+                for p in range(2):
+                  b7 = vector[7]
+                  for q in range(2):
+                    b8 = vector[8]
+                    for r in range(2):
+                      b9 = vector[9]
+                      yield b0[i] + b1[j] + b2[k] + b3[l] + b4[m] + b5[n] + b6[o] + b7[p] + b8[q] + b9[r]
+
+def coll2048(vector):
+  print('  2048col')
+  for i in range(2):
+    b0 = vector[0]
+    for j in range(2):
+      b1 = vector[1]
+      for k in range(2):
+        b2 = vector[2]
+        for l in range(2):
+          b3 = vector[3]
+          for m in range(2):
+            b4 = vector[4]
+            for n in range(2):
+              b5 = vector[5]
+              for o in range(2):
+                b6 = vector[6]
+                for p in range(2):
+                  b7 = vector[7]
+                  for q in range(2):
+                    b8 = vector[8]
+                    for r in range(2):
+                      b9 = vector[9]
+                      for s in range(2):
+                        b10 = vector[10]
+                        yield b0[i] + b1[j] + b2[k] + b3[l] + b4[m] + b5[n] + b6[o] + b7[p] + b8[q] + b9[r] + b10[s]
+'''
+
+def coll4096(vector):
+  print('  4096col')
+  for i in range(2):
+    b0 = vector[0]
+    for j in range(2):
+      b1 = vector[1]
+      for k in range(2):
+        b2 = vector[2]
+        for l in range(2):
+          b3 = vector[3]
+          for m in range(2):
+            b4 = vector[4]
+            for n in range(2):
+              b5 = vector[5]
+              for o in range(2):
+                b6 = vector[6]
+                for p in range(2):
+                  b7 = vector[7]
+                  for q in range(2):
+                    b8 = vector[8]
+                    for r in range(2):
+                      b9 = vector[9]
+                      for s in range(2):
+                        b10 = vector[10]
+                        for t in range(2):
+                          b11 = vector[11]
+                          yield b0[i] + b1[j] + b2[k] + b3[l] + b4[m] + b5[n] + b6[o] + b7[p] + b8[q] + b9[r] + b10[s] + b11[t]
+
+def coll8192(vector):
+  print('  8192col')
+  for i in range(2):
+    b0 = vector[0]
+    for j in range(2):
+      b1 = vector[1]
+      for k in range(2):
+        b2 = vector[2]
+        for l in range(2):
+          b3 = vector[3]
+          for m in range(2):
+            b4 = vector[4]
+            for n in range(2):
+              b5 = vector[5]
+              for o in range(2):
+                b6 = vector[6]
+                for p in range(2):
+                  b7 = vector[7]
+                  for q in range(2):
+                    b8 = vector[8]
+                    for r in range(2):
+                      b9 = vector[9]
+                      for s in range(2):
+                        b10 = vector[10]
+                        for t in range(2):
+                          b11 = vector[11]
+                          for u in range(2):
+                            b12 = vector[12]
+                            yield b0[i] + b1[j] + b2[k] + b3[l] + b4[m] + b5[n] + b6[o] + b7[p] + b8[q] + b9[r] + b10[s] + b11[t] + b12[u]
+
+def coll16384(vector):
+  print('  16384col')
+  for i in range(2):
+    b0 = vector[0]
+    for j in range(2):
+      b1 = vector[1]
+      for k in range(2):
+        b2 = vector[2]
+        for l in range(2):
+          b3 = vector[3]
+          for m in range(2):
+            b4 = vector[4]
+            for n in range(2):
+              b5 = vector[5]
+              for o in range(2):
+                b6 = vector[6]
+                for p in range(2):
+                  b7 = vector[7]
+                  for q in range(2):
+                    b8 = vector[8]
+                    for r in range(2):
+                      b9 = vector[9]
+                      for s in range(2):
+                        b10 = vector[10]
+                        for t in range(2):
+                          b11 = vector[11]
+                          for u in range(2):
+                            b12 = vector[12]
+                            for v in range(2):
+                              b13 = vector[13]
+                              yield b0[i] + b1[j] + b2[k] + b3[l] + b4[m] + b5[n] + b6[o] + b7[p] + b8[q] + b9[r] + b10[s] + b11[t] + b12[u] + b13[v]
+
+def coll32768(vector):
+  print('  32768col')
+  for i in range(2):
+    b0 = vector[0]
+    for j in range(2):
+      b1 = vector[1]
+      for k in range(2):
+        b2 = vector[2]
+        for l in range(2):
+          b3 = vector[3]
+          for m in range(2):
+            b4 = vector[4]
+            for n in range(2):
+              b5 = vector[5]
+              for o in range(2):
+                b6 = vector[6]
+                for p in range(2):
+                  b7 = vector[7]
+                  for q in range(2):
+                    b8 = vector[8]
+                    for r in range(2):
+                      b9 = vector[9]
+                      for s in range(2):
+                        b10 = vector[10]
+                        for t in range(2):
+                          b11 = vector[11]
+                          for u in range(2):
+                            b12 = vector[12]
+                            for v in range(2):
+                              b13 = vector[13]
+                              for w in range(2):
+                                b14 = vector[14]
+                                yield b0[i] + b1[j] + b2[k] + b3[l] + b4[m] + b5[n] + b6[o] + b7[p] + b8[q] + b9[r] + b10[s] + b11[t] + b12[u] + b13[v] + b14[w]
+
+'''
+#def trampoline(collisions):
+#  L = len(collisions)
+#  COUNT = 1
+#  for i in range(2, L + 1):
+#  for i in range(4, 5):
+
+    print(str(pow(2, i)) + 'start')
+#    if i == 2:
+#      collider = coll4(collisions[:i])
+#    elif i == 3:
+#      collider = coll8(collisions[:i])
+#    if i == 4:
+#      collider = coll16(collisions[:i])
+#    elif i == 5:
+#      collider = coll32(collisions[:i])
+#    elif i == 6:
+#      collider = coll64(collisions[:i])
+#    elif i == 7:
+#      collider = coll128(collisions[:i])
+#    elif i == 8:
+#      collider = coll256(collisions[:i])
+#    elif i == 9:
+#      collider = coll512(collisions[:i])
+#    elif i == 10:
+#      collider = coll1024(collisions[:i])
+#    elif i == 11:
+#      collider = coll2048(collisions[:i])
+#    elif i == 12:
+#      collider = coll4096(collisions[:i])
+#    elif i == 13:
+#      collider = coll8192(collisions[:i])
+#    elif i == 14:
+#      collider = coll16384(collisions[:i])
+#    elif i == 15:
+#      collider = coll32768(collisions[:i])
+    else:
+      print('kaboom')
+      exit(255)
+
+    innercount = 0
+    while True:
+      try:
+        c_left = next(collider)
+        c_right = next(collider)
+#        if COUNT > 16375:
+        print('querying\n', c_left, '\n', c_right, end='\n\n')
+        innercount += 1
+        COUNT += 1
+        if s52.g(c_left) == s52.g(c_right):
+          print('found', c_left, c_right, '\ncount', COUNT)
+          exit()
+      except StopIteration:
+        print(str(pow(2, i)) + '-collisions tested out ; total count', COUNT, 'inner', innercount, '\n')
+        break
+
+  return COUNT
+''' 
